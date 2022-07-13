337c93dc9d19a22b5c2284cb9fdb40a3cc136a03
Grzegorz Golonka <golon338@gmail.com>
2022-07-13 21:27:19 +0200

    functional loop file

--- cpp_project/loop.cpp
+++ cpp_project/loop.cpp
@@ -7,37 +7,36 @@ void second_function();
 
 int main()
 {
-    int a,b;
+    int a, b;
     cout << "give two numbers" << endl;
     cin >>
-	   a
-	  >> b;
-    int loopsum = function(a,b);
-    cout<< "\nloop sum: " << endl << loopsum;
-	int x = ten();
-	cout << x <<endl;
-	
+        a >> b;
+    int loopsum = function(a, b);
+    cout << "\nloop sum: " << endl
+         << loopsum;
+    int x = ten();
+    cout << endl
+         << x << endl;
 
     second_function();
-
 }
 int function(int a, int b)
 {
     int counter;
     for (int i = a; i < b; i++)
-        counter+=a;
+        counter += a;
 
     return counter;
 }
 
 int ten()
 {
-	int x = 10;
-	std::cout <<"Siemano kolano";
-	cout << "siemano inaczej;
-	return x;
+    int x = 10;
+    cout << "\nSiemano kolano" << endl
+         << "siemano inaczej" << endl;
+    return x;
+}
 void second_function()
 {
-	cout <<"Siemano kolano";
+    cout << "Siemano kolano";
 }
-
